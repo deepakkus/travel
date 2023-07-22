7be8e31a0d2f78f30f31c2e736d74ca1f0179ae2
deepak.kus <deepak09.singh@gmail.com>
2023-07-22 16:19:44 +0530

    Api Code Updated

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -7,17 +7,28 @@ router.get('/', function(req, res, next) {
 });
 const AdminController = require('../../../Controller/Auth/Admin');
 const TripController = require('../../../Controller/Admin/Trip');
+const BannerController = require('../../../Controller/Admin/Banner');
 router.post('/admin/login', AdminController.login);
 router.post('/admin/register', AdminController.register);
 router.post('/admin/trip', TripController.create);
+router.post('/admin/banner', BannerController.create);
+
 router.put('/admin/trip/:id', TripController.update);
+router.put('/admin/banner/:id', BannerController.update);
+
 router.delete('/admin/trip/:id', TripController.Delete);
+router.delete('/admin/banner/:id', BannerController.Delete);
+
 router.get('/admin/trip-details/:id', TripController.viewSingel);
+router.get('/admin/banner-details/:id', BannerController.viewSingel);
+
 router.get('/admin/trip', TripController.viewAll);
+router.get('/admin/banner', BannerController.viewAll);
 const multer = require('multer');
 const middleware  = require('../../../service/middleware').middleware;
 var storage = multer.memoryStorage()
 var upload = multer({storage: storage});
 router.use(middleware);
 router.post('/admin/trip/upload', upload.single("image"), TripController.ImageUpload);
+router.post('/admin/banner/upload', upload.single("image"), BannerController.ImageUpload);
 module.exports = router;
